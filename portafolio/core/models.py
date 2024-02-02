9566290e7fd76fc0f5e406fc18c35964a82062eb
Williams392 <williamsvalle392@gmail.com>
2024-02-02 00:21:19 -0500

    new en admin Portafolio

--- portafolio/core/models.py
+++ portafolio/core/models.py
@@ -9,4 +9,19 @@ class Proyecto:
         self.imagen=imagen
         self.costo=costo
 
+# instalar libreria para img en django: pip install pillow
+class MiPortafolio(models. Model):
+    proyecto = models.CharField(max_length=120)
+    descripcion = models.TextField()
+    imagen = models.ImageField(upload_to="proyecto_img/", default="proyecto_img/img_2.jng")
+    lenguaje = models.CharField(max_length=120)
+    tecnologia = models.CharField(max_length=120, blank=True, null=True)
+    año = models.IntegerField(blank=True, null=True)
 
+    def __str__(self):
+        return self.proyecto
+
+
+
+# python manage.py makemigrations
+# python manage.py migrate
--- portafolio/core/views.py
+++ portafolio/core/views.py
@@ -1,27 +1,26 @@
 # 1
 
 from django.shortcuts import render
-from .models import Proyecto
+from .models import Proyecto, MiPortafolio
 
 def index(request):
     template_name = "index.html"
 
     # Lista:
-    proyectos = [
-        Proyecto("Proyecto1", "Descripcion del Proyecto 1", "img_1.jpeg", 120),
-        Proyecto("Proyecto2", "Descripcion del Proyecto 2", "img_2.png", 180),
-        Proyecto("Proyecto3", "Descripcion del Proyecto 3", "img_1.jpeg", 200),
-        Proyecto("Proyecto4", "Descripcion del Proyecto 4", "img_2.png", 140)
-        # agregar mas proyectos segun sea necesario
-    ]
+    # proyectos = [
+    #     Proyecto("Proyecto1", "Descripcion del Proyecto 1", "img_1.jpeg", 120),
+    #     Proyecto("Proyecto2", "Descripcion del Proyecto 2", "img_2.png", 180),
+    #     Proyecto("Proyecto3", "Descripcion del Proyecto 3", "img_1.jpeg", 200),
+    #     Proyecto("Proyecto4", "Descripcion del Proyecto 4", "img_2.png", 140)
+    #     # agregar mas proyectos segun sea necesario
+    # ]
+
+    miportafolio = MiPortafolio.objects.all() # select * fromn MiPortafolio
+
     context = { # para pasar al templates
-        'proyectos':proyectos
+        'proyectos':miportafolio
     }
 
     return render(request, template_name, context)
-
-
-
-
 
 
