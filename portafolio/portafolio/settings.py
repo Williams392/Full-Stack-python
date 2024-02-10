67b4f1028a7ec13b3c9d4c9f0494329a8319b7dd
Williams392 <williamsvalle392@gmail.com>
2024-02-09 22:40:12 -0500

    mejorando el index

--- portafolio/portafolio/settings.py
+++ portafolio/portafolio/settings.py
@@ -76,12 +76,24 @@ WSGI_APPLICATION = 'portafolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+# comando para usar postgreSQL: pip install psycopg2
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'portafolio',
+        'USER':'williams392',
+        'PASSWORD':'1234',
+        'HOST':'localhost',
+        'PORT':'5432'
     }
 }
+
 
 
 # Password validation
